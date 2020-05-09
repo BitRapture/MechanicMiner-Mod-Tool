31851bffeee0b4b483af251c2388a507486284d9
BitRapture <thinzy.business+bitrapture@gmail.com>
2020-05-09 18:07:31 +0100

    Enable repacking and dumping

--- MechanicMiner Mod Tool/cMain.cpp
+++ MechanicMiner Mod Tool/cMain.cpp
@@ -4,6 +4,7 @@ wxBEGIN_EVENT_TABLE(cMain, wxFrame)
 
 EVT_BUTTON(10002, cMain::open_pressed)
 EVT_BUTTON(10004, cMain::dir_pressed)
+EVT_BUTTON(10006, cMain::func_pressed)
 
 wxEND_EVENT_TABLE()
 
@@ -21,12 +22,19 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "MM Mod Tool", wxPoint(200, 200), wx
 	txt_open_status->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
 	txt_open_status->SetForegroundColour(wxColour(209, 10, 10));
 
-	txt_dir = new wxStaticText(this, wxID_ANY, "SELECT WORKSPACE FOLDER", wxPoint(4, 51));
+	txt_dir = new wxStaticText(this, wxID_ANY, "SELECT WORKSPACE FOLDER", wxPoint(4, 50));
 	txt_dir->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
 	txt_dir->SetForegroundColour(wxColour(173, 190, 201));
-	txt_dir_status = new wxStaticText(this, wxID_ANY, "DISCONNECTED", wxPoint(164, 51));
+	txt_dir_status = new wxStaticText(this, wxID_ANY, "DISCONNECTED", wxPoint(164, 50));
 	txt_dir_status->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
 	txt_dir_status->SetForegroundColour(wxColour(209, 10, 10));
+
+	txt_func = new wxStaticText(this, wxID_ANY, "ASSET OPTIONS", wxPoint(4, 96));
+	txt_func->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
+	txt_func->SetForegroundColour(wxColour(173, 190, 201));
+	txt_func_status = new wxStaticText(this, wxID_ANY, "DISABLED", wxPoint(94, 96));
+	txt_func_status->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
+	txt_func_status->SetForegroundColour(wxColour(209, 10, 10));
 
 	ctrl_open = new wxTextCtrl(this, 10001, wxEmptyString, wxPoint(4, 20), wxSize(366, 20));
 	ctrl_open->SetBackgroundColour(wxColour(23, 26, 28));
@@ -46,14 +54,19 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "MM Mod Tool", wxPoint(200, 200), wx
 	btn_dir->SetBackgroundColour(wxColour(173, 190, 201));
 	btn_dir->SetForegroundColour(wxColour(23, 26, 28));
 
+	btn_func = new wxButton(this, 10006, "SELECT", wxPoint(4, 232), wxSize(426, 22));
+	btn_func->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
+	btn_func->SetBackgroundColour(wxColour(173, 190, 201));
+	btn_func->SetForegroundColour(wxColour(23, 26, 28));
+	btn_func->Disable();
+
 	list_funcs = new wxListBox(this, 10005, wxPoint(4, 112), wxSize(426, 120));
 	list_funcs->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
 	list_funcs->SetBackgroundColour(wxColour(23, 26, 28));
 	list_funcs->SetForegroundColour(wxColour(173, 190, 201));
 	wxArrayString functions;
-	functions.Add("DUMP ALL ASSETS");
-	functions.Add("DUMP JOHN SPRITES");
-	functions.Add("DUMP HOLOJOHN SPRITES");
+	functions.Add("REPACK ASSET FILE");
+	functions.Add("DUMP ALL SPRITE ASSETS");
 	list_funcs->Append(functions);
 	list_funcs->Disable();
 }
@@ -61,6 +74,8 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "MM Mod Tool", wxPoint(200, 200), wx
 cMain::~cMain()
 {
 	asset_loc.clear();
+	temp_hold.clear();
+	free(asset_hold);
 }
 
 void cMain::check_enable()
@@ -68,7 +83,106 @@ void cMain::check_enable()
 	if (asset_loaded && directory_saved)
 	{
 		list_funcs->Enable();
+		btn_func->Enable();
+		txt_func_status->SetForegroundColour(wxColour(30, 189, 33));
+		txt_func_status->SetLabelText(std::to_string(asset_loc.size()) + " ASSETS FOUND");
 	}
+}
+
+void cMain::repack()
+{
+	temp_hold.clear();
+	unsigned int i = 0, p = 0, endp = 0;
+	txt_func_status->SetForegroundColour(wxColour(247, 203, 27));
+	txt_func_status->SetLabelText("REPACKING ASSETS");
+	while (i < asset_size)
+	{
+		if (i + 4 < asset_size)
+		{
+			if (asset_hold[i] == 0x89 && asset_hold[i + 1] == 0x50 && asset_hold[i + 2] == 0x4E && asset_hold[i + 3] == 0x47)
+			{
+				if (dir_path.HasFiles("asset_" + std::to_string(p) + ".png"))
+				{
+					endp = 0;
+					while (1)
+					{
+						if (asset_hold[i + endp] == 0x49 && asset_hold[i + endp + 1] == 0x45 && asset_hold[i + endp + 2] == 0x4E && asset_hold[i + endp + 3] == 0x44 && asset_hold[i + endp + 4] == 0xAE && asset_hold[i + endp + 5] == 0x42 && asset_hold[i + endp + 6] == 0x60 && asset_hold[i + endp + 7] == 0x82)
+						{
+							endp += 8;
+							break;
+						}
+						endp++;
+					}
+
+					wxFFileInputStream pack(dir_path.GetName() + "\\asset_" + std::to_string(p) + ".png");
+					UINT8* pack_hold = (UINT8*)malloc(pack.GetSize());
+					if (pack_hold == nullptr) return;
+					unsigned int pack_size = pack.GetSize();
+					pack.ReadAll(pack_hold, pack.GetSize());
+
+					for (unsigned int ph = 0; ph < pack_size; ph++)
+					{
+						temp_hold.push_back(pack_hold[ph]);
+					}
+					free(pack_hold);
+					i += endp;
+				}
+				p++;
+			}
+		}
+		temp_hold.push_back(asset_hold[i]);
+		i++;
+	}
+
+	wxFFileOutputStream packed_file(dir_path.GetName() + "\\main.mechanicminer_asset_64");
+	UINT8* buffer = (UINT8*)malloc(temp_hold.size());
+	if (buffer == nullptr) return;
+	for (unsigned int ii = 0; ii < temp_hold.size(); ii++) { buffer[ii] = temp_hold[ii]; }
+
+	packed_file.WriteAll(buffer, temp_hold.size());
+	free(buffer);
+	temp_hold.clear();
+	txt_func_status->SetForegroundColour(wxColour(30, 189, 33));
+	txt_func_status->SetLabelText("ASSET FILE REPACKED");
+}
+
+void cMain::dump_all()
+{
+	unsigned int p = 0;
+	txt_func_status->SetForegroundColour(wxColour(247, 203, 27));
+	txt_func_status->SetLabelText("DUMPING 0/" + std::to_string(asset_loc.size()) + "ASSETS");
+	for (unsigned int i = 0; i < asset_loc.size(); i++)
+	{
+		wxFFileOutputStream asset(dir_path.GetName() + "\\asset_" + std::to_string(i) + ".png");
+		txt_func_status->SetLabelText("DUMPING " + std::to_string(i + 1) + "/" + std::to_string(asset_loc.size()) + " ASSETS");
+		temp_hold.clear();
+		p = 0;
+		while (1)
+		{
+			temp_hold.push_back(asset_hold[asset_loc[i] + p]);
+			if (asset_hold[asset_loc[i] + p] == 0x49 && asset_hold[asset_loc[i] + p + 1] == 0x45 && asset_hold[asset_loc[i] + p + 2] == 0x4E && asset_hold[asset_loc[i] + p + 3] == 0x44 && asset_hold[asset_loc[i] + p + 4] == 0xAE && asset_hold[asset_loc[i] + p + 5] == 0x42 && asset_hold[asset_loc[i] + p + 6] == 0x60 && asset_hold[asset_loc[i] + p + 7] == 0x82)
+			{
+				temp_hold.push_back(0x45);
+				temp_hold.push_back(0x4E);
+				temp_hold.push_back(0x44);
+				temp_hold.push_back(0xAE);
+				temp_hold.push_back(0x42);
+				temp_hold.push_back(0x60);
+				temp_hold.push_back(0x82);
+				break;
+			}
+			p++;
+		}
+		UINT8* buffer = (UINT8*)malloc(temp_hold.size());
+		if (buffer == nullptr) return;
+		for (unsigned int ii = 0; ii < temp_hold.size(); ii++) { buffer[ii] = temp_hold[ii]; }
+		asset.WriteAll(buffer, temp_hold.size());
+		free(buffer);
+	}
+	temp_hold.clear();
+
+	txt_func_status->SetForegroundColour(wxColour(30, 189, 33));
+	txt_func_status->SetLabelText("ALL ASSETS DUMPED");
 }
 
 void cMain::open_pressed(wxCommandEvent& evt)
@@ -164,4 +278,27 @@ void cMain::dir_pressed(wxCommandEvent& evt)
 
 		evt.Skip();
 	}
+}
+
+void cMain::func_pressed(wxCommandEvent& evt)
+{
+	if (list_funcs->GetSelection() == wxNOT_FOUND) { evt.Skip(); return; }
+
+	wxString selected = list_funcs->GetString(list_funcs->GetSelection());
+
+	if (selected == "REPACK ASSET FILE")
+	{
+		btn_func->Disable();
+		repack();
+		btn_func->Enable();
+	}
+
+	if (selected == "DUMP ALL SPRITE ASSETS")
+	{
+		btn_func->Disable();
+		dump_all();
+		btn_func->Enable();
+	}
+
+	evt.Skip();
 }
--- MechanicMiner Mod Tool/cMain.h
+++ MechanicMiner Mod Tool/cMain.h
@@ -12,10 +12,11 @@ private:
 	unsigned int asset_size;
 	std::vector<unsigned int> asset_loc;
 	UINT8* asset_hold;
+	std::vector<UINT8> temp_hold;
 
-	wxStaticText* txt_open, *txt_open_status, *txt_dir, *txt_dir_status;
+	wxStaticText* txt_open, *txt_open_status, *txt_dir, *txt_dir_status, *txt_func, *txt_func_status;
 	wxTextCtrl* ctrl_open, *ctrl_dir;
-	wxButton* btn_open, *btn_dir;
+	wxButton* btn_open, *btn_dir, *btn_func;
 	wxListBox* list_funcs;
 
 	wxDir dir_path;
@@ -26,8 +27,12 @@ public:
 
 	void check_enable();
 
+	void repack();
+	void dump_all();
+
 	void open_pressed(wxCommandEvent& evt);
 	void dir_pressed(wxCommandEvent& evt);
+	void func_pressed(wxCommandEvent& evt);
 
 	wxDECLARE_EVENT_TABLE();
 };

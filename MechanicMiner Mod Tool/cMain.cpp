e665f6b4b80223e982b9d291f2c5f5cbb6f5c14c
BitRapture <thinzy.business+bitrapture@gmail.com>
2020-05-27 00:10:04 +0100

    start of texture packs

--- MechanicMiner Mod Tool/cMain.cpp
+++ MechanicMiner Mod Tool/cMain.cpp
@@ -5,13 +5,35 @@ wxBEGIN_EVENT_TABLE(cMain, wxFrame)
 EVT_BUTTON(10002, cMain::open_pressed)
 EVT_BUTTON(10004, cMain::dir_pressed)
 EVT_BUTTON(10006, cMain::func_pressed)
+EVT_BUTTON(10008, cMain::open2_pressed)
 
 wxEND_EVENT_TABLE()
 
-cMain::cMain() : wxFrame(nullptr, wxID_ANY, "MM Mod Tool", wxPoint(200, 200), wxSize(450, 300), wxSYSTEM_MENU | wxCAPTION | wxCLOSE_BOX | wxCLIP_CHILDREN | wxSTAY_ON_TOP)
+/*
+	ID LIST
+	-----------------------------------
+	Asset Txtbox =				10001,
+	Asset Open Btn =			10002,
+	Directory Txtbox =			10003,
+	Directory Select Btn =		10004,
+	Function box =				10005,
+	Function Select Btn =		10006,
+	Asset links Txtbox =		10007,
+	Asset links Btn =			10008,
+*/
+
+/*
+	DEBUG copy-paste:
+C:\Users\FiercePC\Desktop\main.mechanicminer_asset_64
+C:\Users\FiercePC\Desktop\main.mechanicminer_asset_64_links
+C:\Users\FiercePC\Desktop\Template
+*/
+
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "MM Mod Tool (v1.1)", wxPoint(200, 200), wxSize(450, 340), wxSYSTEM_MENU | wxCAPTION | wxCLOSE_BOX | wxCLIP_CHILDREN | wxSTAY_ON_TOP)
 {
 	asset_loaded = false;
 	directory_saved = false;
+	link_loaded = false;
 	
 	SetBackgroundColour(wxColour(23, 26, 28));
 
@@ -22,17 +44,24 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "MM Mod Tool", wxPoint(200, 200), wx
 	txt_open_status->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
 	txt_open_status->SetForegroundColour(wxColour(209, 10, 10));
 
-	txt_dir = new wxStaticText(this, wxID_ANY, "SELECT WORKSPACE FOLDER", wxPoint(4, 50));
+	txt_open2 = new wxStaticText(this, wxID_ANY, "OPEN LINKS FILE (MECHANICMINER_ASSET_64_LINKS)", wxPoint(4, 50));
+	txt_open2->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
+	txt_open2->SetForegroundColour(wxColour(173, 190, 201));
+	txt_open2_status = new wxStaticText(this, wxID_ANY, "UNLOADED", wxPoint(292, 50));
+	txt_open2_status->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
+	txt_open2_status->SetForegroundColour(wxColour(209, 10, 10));
+
+	txt_dir = new wxStaticText(this, wxID_ANY, "SELECT WORKSPACE FOLDER", wxPoint(4, 96)); 
 	txt_dir->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
 	txt_dir->SetForegroundColour(wxColour(173, 190, 201));
-	txt_dir_status = new wxStaticText(this, wxID_ANY, "DISCONNECTED", wxPoint(164, 50));
+	txt_dir_status = new wxStaticText(this, wxID_ANY, "DISCONNECTED", wxPoint(164, 96));
 	txt_dir_status->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
 	txt_dir_status->SetForegroundColour(wxColour(209, 10, 10));
 
-	txt_func = new wxStaticText(this, wxID_ANY, "ASSET OPTIONS", wxPoint(4, 96));
+	txt_func = new wxStaticText(this, wxID_ANY, "ASSET OPTIONS", wxPoint(4, 142));
 	txt_func->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
 	txt_func->SetForegroundColour(wxColour(173, 190, 201));
-	txt_func_status = new wxStaticText(this, wxID_ANY, "DISABLED", wxPoint(94, 96));
+	txt_func_status = new wxStaticText(this, wxID_ANY, "DISABLED", wxPoint(94, 142));
 	txt_func_status->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
 	txt_func_status->SetForegroundColour(wxColour(209, 10, 10));
 
@@ -40,7 +69,11 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "MM Mod Tool", wxPoint(200, 200), wx
 	ctrl_open->SetBackgroundColour(wxColour(23, 26, 28));
 	ctrl_open->SetForegroundColour(wxColour(173, 190, 201));
 
-	ctrl_dir = new wxTextCtrl(this, 10003, wxEmptyString, wxPoint(4, 66), wxSize(366, 20));
+	ctrl_open2 = new wxTextCtrl(this, 10007, wxEmptyString, wxPoint(4, 66), wxSize(366, 20));
+	ctrl_open2->SetBackgroundColour(wxColour(23, 26, 28));
+	ctrl_open2->SetForegroundColour(wxColour(173, 190, 201));
+
+	ctrl_dir = new wxTextCtrl(this, 10003, wxEmptyString, wxPoint(4, 112), wxSize(366, 20));
 	ctrl_dir->SetBackgroundColour(wxColour(23, 26, 28));
 	ctrl_dir->SetForegroundColour(wxColour(173, 190, 201));
 
@@ -49,23 +82,29 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "MM Mod Tool", wxPoint(200, 200), wx
 	btn_open->SetBackgroundColour(wxColour(173, 190, 201));
 	btn_open->SetForegroundColour(wxColour(23, 26, 28));
 
-	btn_dir = new wxButton(this, 10004, "SELECT", wxPoint(370, 65), wxSize(60, 22));
+	btn_open2 = new wxButton(this, 10008, "OPEN", wxPoint(370, 65), wxSize(60, 22));
+	btn_open2->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
+	btn_open2->SetBackgroundColour(wxColour(173, 190, 201));
+	btn_open2->SetForegroundColour(wxColour(23, 26, 28));
+
+	btn_dir = new wxButton(this, 10004, "SELECT", wxPoint(370, 111), wxSize(60, 22));
 	btn_dir->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
 	btn_dir->SetBackgroundColour(wxColour(173, 190, 201));
 	btn_dir->SetForegroundColour(wxColour(23, 26, 28));
 
-	btn_func = new wxButton(this, 10006, "SELECT", wxPoint(4, 232), wxSize(426, 22));
+	btn_func = new wxButton(this, 10006, "SELECT", wxPoint(4, 278), wxSize(426, 22));
 	btn_func->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
 	btn_func->SetBackgroundColour(wxColour(173, 190, 201));
 	btn_func->SetForegroundColour(wxColour(23, 26, 28));
 	btn_func->Disable();
 
-	list_funcs = new wxListBox(this, 10005, wxPoint(4, 112), wxSize(426, 120));
+	list_funcs = new wxListBox(this, 10005, wxPoint(4, 158), wxSize(426, 120));
 	list_funcs->SetFont(wxFont(8, wxFONTFAMILY_SWISS, wxFONTSTYLE_MAX, wxFONTWEIGHT_BOLD));
 	list_funcs->SetBackgroundColour(wxColour(23, 26, 28));
 	list_funcs->SetForegroundColour(wxColour(173, 190, 201));
 	wxArrayString functions;
-	functions.Add("REPACK ASSET FILE");
+	functions.Add("REPACK ASSET FILE (.MMTP)");
+	functions.Add("REPACK ASSET FILE (RAW)");
 	functions.Add("DUMP ALL SPRITE ASSETS");
 	list_funcs->Append(functions);
 	list_funcs->Disable();
@@ -73,47 +112,187 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "MM Mod Tool", wxPoint(200, 200), wx
 
 cMain::~cMain()
 {
-	asset_loc.clear();
+	sprite_loc.clear();
+	sprite_link.clear();
+	sprite_temp_size.clear();
 	temp_hold.clear();
+	temp2_hold.clear();
+	asset_links.clear();
 	free(asset_hold);
+	free(link_hold);
+}
+
+unsigned long int cMain::reverse_read(UINT8* input, int size, int offset)
+{
+	std::stringstream hexstream;
+	char hold[2];
+	for (int i = size; i > -1; i--)
+	{
+		itoa(input[i + offset], hold, 16);
+		hexstream << std::setfill('0') << std::setw(2) << hold;
+	} 
+	return static_cast<unsigned>(strtoul(hexstream.str().c_str(), NULL, 16));
+}
+unsigned long int cMain::reverse_read(std::vector<UINT8> input, int size, int offset)
+{
+	std::stringstream hexstream;
+	char hold[2];
+	for (int i = size; i > -1; i--)
+	{
+		itoa(input[i + offset], hold, 16);
+		hexstream << std::setfill('0') << std::setw(2) << hold;
+	}
+	return static_cast<unsigned>(strtoul(hexstream.str().c_str(), NULL, 16));
+}
+
+std::vector<UINT8> cMain::split_hex(unsigned long int input)
+{
+	std::vector<UINT8> output;
+	char hold[16], hex[3];
+	itoa(input, hold, 16);
+	int i = 1;
+	hex[2] = '\0';
+	while (i < 16)
+	{
+		hex[0] = '0';
+		if (i - 1 < strlen(hold)) hex[0] = hold[i - 1];
+		hex[1] = '0';
+		if (i < strlen(hold))
+		{
+			hex[1] = hold[i];
+		}
+		else if (hex[0] != '0') {
+			hex[1] = hex[0];
+			hex[0] = '0';
+		}
+
+		output.push_back(static_cast<UINT8>(strtoul(hex, NULL, 16)));
+		i += 2;
+	}
+	std::reverse(output.begin(), output.end());
+	return output;
 }
 
 void cMain::check_enable()
 {
-	if (asset_loaded && directory_saved)
+	if (asset_loaded && link_loaded && directory_saved)
 	{
+		txt_func_status->SetForegroundColour(wxColour(247, 203, 27));
+		txt_func_status->SetLabelText(wxString("LINKING OFFSETS"));
+
+		// Link up asset offsets
+		unsigned long int link_offset, asset_offset, i = 0;
+		while(i < link_size)
+		{
+			link_offset = reverse_read(link_hold, 7, i);
+			asset_offset = reverse_read(asset_hold, 7, link_offset);
+			
+			asset_links.push_back(asset_offset);
+
+			if (asset_hold[asset_offset + 4] == 0x89 && asset_hold[asset_offset + 5] == 0x50 && asset_hold[asset_offset + 6] == 0x4E && asset_hold[asset_offset + 7] == 0x47)
+			{
+				sprite_link.push_back(link_offset);
+			}
+			i += 8;
+		}
+
 		list_funcs->Enable();
 		btn_func->Enable();
 		txt_func_status->SetForegroundColour(wxColour(30, 189, 33));
-		txt_func_status->SetLabelText(std::to_string(asset_loc.size()) + " ASSETS FOUND");
+		txt_func_status->SetLabelText("LINKED " + std::to_string(sprite_link.size()) + " ASSETS");
 	}
+}
+
+void cMain::relink_offsets()
+{
+	// TO-DO:
+	/*
+		get_offset needs to check the asset links
+		the links need to point to 4 bytes before a PNG header
+		if they dont, compare size of original asset_64 to this one
+		then while-loop until the link points to the correct PNG
+		save this new link
+
+		then in assets_64_links edit link to new asset link loc
+
+		export both new files
+		get on knees and pray
+	*/
+
+	unsigned long int i = 0, ii = 0, ts = 0, sprite_accum = 0, link_offset, casset_offset;
+	int size_diff;
+	while (i < link_size)
+	{
+		link_offset = reverse_read(link_hold, 7, i);
+		casset_offset = reverse_read(temp_hold, 7, link_offset);
+
+		if (casset_offset != asset_links[ii])
+		{
+			if (sprite_link[ts] == link_offset)
+			{
+				sprite_accum += sprite_temp2_size[ts];
+				if (sprite_temp_size[ts] != sprite_temp2_size[ts])
+				{
+					if (sprite_temp2_size[ts] < sprite_temp_size[ts])
+					{
+						size_diff = 1;
+					}
+					else {
+						size_diff = -1;
+					}
+					while (1)
+					{
+
+					}
+				}
+			}
+			ts++;
+		}
+
+		std::vector<UINT8> new_link = split_hex(link_offset);
+		for (int nl = 1; nl < 9; nl++)
+		{
+			temp2_hold.push_back(new_link[8 - nl]);
+		}
+
+		i += 8;
+		ii++;
+	}
+
+	sprite_temp_size.clear();
+	std::vector<unsigned long int>().swap(sprite_temp_size);
+	sprite_temp2_size.clear();
+	std::vector<unsigned long int>().swap(sprite_temp2_size);
 }
 
 void cMain::repack()
 {
 	temp_hold.clear();
-	unsigned int i = 0, p = 0, endp = 0;
+	std::vector<UINT8>().swap(temp_hold);
+	unsigned int i = 0, p, endp;
 	txt_func_status->SetForegroundColour(wxColour(247, 203, 27));
-	txt_func_status->SetLabelText("REPACKING ASSETS");
+	txt_func_status->SetLabelText(wxString("REPACKING ASSETS"));
 	while (i < asset_size)
 	{
-		if (i + 4 < asset_size)
+		if (i + 3 < asset_size)
 		{
+			// Repack PNGs
 			if (asset_hold[i] == 0x89 && asset_hold[i + 1] == 0x50 && asset_hold[i + 2] == 0x4E && asset_hold[i + 3] == 0x47)
 			{
+				endp = 0;
+				while (1)
+				{
+					if (asset_hold[i + endp] == 0x49 && asset_hold[i + endp + 1] == 0x45 && asset_hold[i + endp + 2] == 0x4E && asset_hold[i + endp + 3] == 0x44 && asset_hold[i + endp + 4] == 0xAE && asset_hold[i + endp + 5] == 0x42 && asset_hold[i + endp + 6] == 0x60 && asset_hold[i + endp + 7] == 0x82)
+					{
+						endp += 8;
+						break;
+					}
+					endp++;
+				}
+				sprite_temp_size.push_back(endp + 6);
+				sprite_temp2_size.push_back(endp + 6);
 				if (dir_path.HasFiles("asset_" + std::to_string(p) + ".png"))
 				{
-					endp = 0;
-					while (1)
-					{
-						if (asset_hold[i + endp] == 0x49 && asset_hold[i + endp + 1] == 0x45 && asset_hold[i + endp + 2] == 0x4E && asset_hold[i + endp + 3] == 0x44 && asset_hold[i + endp + 4] == 0xAE && asset_hold[i + endp + 5] == 0x42 && asset_hold[i + endp + 6] == 0x60 && asset_hold[i + endp + 7] == 0x82)
-						{
-							endp += 8;
-							break;
-						}
-						endp++;
-					}
-
 					wxFFileInputStream pack(dir_path.GetName() + "\\asset_" + std::to_string(p) + ".png");
 					UINT8* pack_hold = (UINT8*)malloc(pack.GetSize());
 					if (pack_hold == nullptr) return;
@@ -125,6 +304,14 @@ void cMain::repack()
 						temp_hold.push_back(pack_hold[ph]);
 					}
 					free(pack_hold);
+
+					std::vector<UINT8> new_size = split_hex(pack_size);
+					for (int ii = 1; ii < 5; ii++)
+					{
+						temp_hold[i - ii] = new_size[4 - ii];
+					}
+					sprite_temp2_size[p] = pack_size + 4;
+
 					i += endp;
 				}
 				p++;
@@ -134,6 +321,8 @@ void cMain::repack()
 		i++;
 	}
 
+	relink_offsets();
+
 	wxFFileOutputStream packed_file(dir_path.GetName() + "\\main.mechanicminer_asset_64");
 	UINT8* buffer = (UINT8*)malloc(temp_hold.size());
 	if (buffer == nullptr) return;
@@ -142,6 +331,8 @@ void cMain::repack()
 	packed_file.WriteAll(buffer, temp_hold.size());
 	free(buffer);
 	temp_hold.clear();
+	std::vector<UINT8>().swap(temp_hold);
+
 	txt_func_status->SetForegroundColour(wxColour(30, 189, 33));
 	txt_func_status->SetLabelText("ASSET FILE REPACKED");
 }
@@ -150,17 +341,18 @@ void cMain::dump_all()
 {
 	unsigned int p = 0;
 	txt_func_status->SetForegroundColour(wxColour(247, 203, 27));
-	txt_func_status->SetLabelText("DUMPING 0/" + std::to_string(asset_loc.size()) + "ASSETS");
-	for (unsigned int i = 0; i < asset_loc.size(); i++)
+	txt_func_status->SetLabelText("DUMPING 0/" + std::to_string(sprite_loc.size()) + "ASSETS");
+	for (unsigned int i = 0; i < sprite_loc.size(); i++)
 	{
 		wxFFileOutputStream asset(dir_path.GetName() + "\\asset_" + std::to_string(i) + ".png");
-		txt_func_status->SetLabelText("DUMPING " + std::to_string(i + 1) + "/" + std::to_string(asset_loc.size()) + " ASSETS");
+		txt_func_status->SetLabelText("DUMPING " + std::to_string(i + 1) + "/" + std::to_string(sprite_loc.size()) + " ASSETS");
 		temp_hold.clear();
+		std::vector<UINT8>().swap(temp_hold);
 		p = 0;
 		while (1)
 		{
-			temp_hold.push_back(asset_hold[asset_loc[i] + p]);
-			if (asset_hold[asset_loc[i] + p] == 0x49 && asset_hold[asset_loc[i] + p + 1] == 0x45 && asset_hold[asset_loc[i] + p + 2] == 0x4E && asset_hold[asset_loc[i] + p + 3] == 0x44 && asset_hold[asset_loc[i] + p + 4] == 0xAE && asset_hold[asset_loc[i] + p + 5] == 0x42 && asset_hold[asset_loc[i] + p + 6] == 0x60 && asset_hold[asset_loc[i] + p + 7] == 0x82)
+			temp_hold.push_back(asset_hold[sprite_loc[i] + p]);
+			if (asset_hold[sprite_loc[i] + p] == 0x49 && asset_hold[sprite_loc[i] + p + 1] == 0x45 && asset_hold[sprite_loc[i] + p + 2] == 0x4E && asset_hold[sprite_loc[i] + p + 3] == 0x44 && asset_hold[sprite_loc[i] + p + 4] == 0xAE && asset_hold[sprite_loc[i] + p + 5] == 0x42 && asset_hold[sprite_loc[i] + p + 6] == 0x60 && asset_hold[sprite_loc[i] + p + 7] == 0x82)
 			{
 				temp_hold.push_back(0x45);
 				temp_hold.push_back(0x4E);
@@ -180,6 +372,7 @@ void cMain::dump_all()
 		free(buffer);
 	}
 	temp_hold.clear();
+	std::vector<UINT8>().swap(temp_hold);
 
 	txt_func_status->SetForegroundColour(wxColour(30, 189, 33));
 	txt_func_status->SetLabelText("ALL ASSETS DUMPED");
@@ -197,7 +390,6 @@ void cMain::open_pressed(wxCommandEvent& evt)
 
 		if (asset.GetLength() < 337895356)
 		{
-
 			txt_open_status->SetForegroundColour(wxColour(209, 10, 10));
 			txt_open_status->SetLabelText("INCORRECT FILE");
 			evt.Skip();
@@ -207,31 +399,31 @@ void cMain::open_pressed(wxCommandEvent& evt)
 		asset_hold = (UINT8*)malloc(asset.GetLength());
 		if (asset_hold == nullptr)
 		{
-
 			txt_open_status->SetForegroundColour(wxColour(209, 10, 10));
-			txt_open_status->SetLabelText("FILE TOO BIG");
+			txt_open_status->SetLabelText("OUT OF MEM");
 			evt.Skip();
 			return;
 		}
 		asset_size = asset.GetLength();
 		asset.ReadAll(asset_hold, asset.GetLength());
 
+		// Check Asset File is the original
 		if (asset_hold[1688] != 0xF0 || asset_hold[4352] != 0xC0 || asset_hold[7194] != 0x5D)
 		{
-
 			txt_open_status->SetForegroundColour(wxColour(209, 10, 10));
 			txt_open_status->SetLabelText("INCORRECT FILE");
 			evt.Skip();
 			return;
 		}
 
+		// Find and save PNGs
 		for (unsigned int i = 0; i < asset_size; i++)
 		{
-			if (i + (unsigned)4 < asset_size)
+			if (i + (unsigned)3 < asset_size)
 			{
 				if (asset_hold[i] == 0x89 && asset_hold[i + 1] == 0x50 && asset_hold[i + 2] == 0x4E && asset_hold[i + 3] == 0x47)
 				{
-					asset_loc.push_back(i);
+					sprite_loc.push_back(i);
 				}
 			}
 		}
@@ -242,6 +434,57 @@ void cMain::open_pressed(wxCommandEvent& evt)
 		txt_open_status->SetLabelText("LOADED");
 		btn_open->Disable();
 		ctrl_open->Disable();
+
+		check_enable();
+
+		evt.Skip();
+	}
+}
+
+void cMain::open2_pressed(wxCommandEvent& evt) 
+{
+	if (!link_loaded)
+	{
+		wxFFileInputStream link(ctrl_open2->GetValue());
+
+		if (!link.IsOk() || !link.CanRead()) { evt.Skip(); return; }
+		txt_open2_status->SetForegroundColour(wxColour(247, 203, 27));
+		txt_open2_status->SetLabelText("LOADING FILE");
+
+		if (link.GetLength() < 88544)
+		{
+			txt_open2_status->SetForegroundColour(wxColour(209, 10, 10));
+			txt_open2_status->SetLabelText("INCORRECT FILE");
+			evt.Skip();
+			return;
+		}
+
+		link_hold = (UINT8*)malloc(link.GetLength());
+		if (link_hold == nullptr)
+		{
+			txt_open2_status->SetForegroundColour(wxColour(209, 10, 10));
+			txt_open2_status->SetLabelText("OUT OF MEM");
+			evt.Skip();
+			return;
+		}
+		link_size = link.GetLength();
+		link.ReadAll(link_hold, link.GetLength());
+
+		// Check Links File is the original
+		if (link_hold[65050] != 0x9E || link_hold[66384] != 0x48 || link_hold[23417] != 0x5B)
+		{
+			txt_open2_status->SetForegroundColour(wxColour(209, 10, 10));
+			txt_open2_status->SetLabelText("INCORRECT FILE");
+			evt.Skip();
+			return;
+		}
+
+		link_loaded = true;
+
+		txt_open2_status->SetForegroundColour(wxColour(30, 189, 33));
+		txt_open2_status->SetLabelText("LOADED");
+		btn_open2->Disable();
+		ctrl_open2->Disable();
 
 		check_enable();
 
@@ -286,7 +529,7 @@ void cMain::func_pressed(wxCommandEvent& evt)
 
 	wxString selected = list_funcs->GetString(list_funcs->GetSelection());
 
-	if (selected == "REPACK ASSET FILE")
+	if (selected == "REPACK ASSET FILE (RAW)")
 	{
 		btn_func->Disable();
 		repack();
--- MechanicMiner Mod Tool/cMain.h
+++ MechanicMiner Mod Tool/cMain.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+#include <sstream>
+#include <iomanip>
 #include "wx/wx.h"
 #include "wx/dir.h"
 #include "wx/wfstream.h"
@@ -8,15 +10,15 @@ class cMain : public wxFrame
 {
 
 private:
-	bool asset_loaded, directory_saved;
-	unsigned int asset_size;
-	std::vector<unsigned int> asset_loc;
-	UINT8* asset_hold;
-	std::vector<UINT8> temp_hold;
+	bool asset_loaded, link_loaded, directory_saved;
+	unsigned int asset_size, link_size;
+	std::vector<unsigned long int> sprite_loc, sprite_link, sprite_temp_size, sprite_temp2_size, asset_links;
+	UINT8* asset_hold, *link_hold;
+	std::vector<UINT8> temp_hold, temp2_hold;
 
-	wxStaticText* txt_open, *txt_open_status, *txt_dir, *txt_dir_status, *txt_func, *txt_func_status;
-	wxTextCtrl* ctrl_open, *ctrl_dir;
-	wxButton* btn_open, *btn_dir, *btn_func;
+	wxStaticText* txt_open, *txt_open2, *txt_open_status, *txt_open2_status, *txt_dir, *txt_dir_status, *txt_func, *txt_func_status;
+	wxTextCtrl* ctrl_open, *ctrl_open2, *ctrl_dir;
+	wxButton* btn_open, *btn_open2, *btn_dir, *btn_func;
 	wxListBox* list_funcs;
 
 	wxDir dir_path;
@@ -25,12 +27,20 @@ public:
 	cMain();
 	~cMain();
 
-	void check_enable();
+	// Global funcs
+	unsigned long int reverse_read(UINT8* input, int size, int offset = 0);
+	unsigned long int reverse_read(std::vector<UINT8> input, int size, int offset = 0);
+	std::vector<UINT8> split_hex(unsigned long int input);
 
+	// Specific funcs
+	void check_enable();
+	void relink_offsets();
 	void repack();
 	void dump_all();
 
+	// Event funcs
 	void open_pressed(wxCommandEvent& evt);
+	void open2_pressed(wxCommandEvent& evt);
 	void dir_pressed(wxCommandEvent& evt);
 	void func_pressed(wxCommandEvent& evt);
 
